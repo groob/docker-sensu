efe1d8d21bc7ac96e8153e38daaa7ccc52f2317c
groob <vrancean+github@gmail.com>
2015-06-23 19:02:25 -0400

    run-client

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,10 +3,14 @@ set -e
 
 /sensu-config 
 
+run-client () { 
+	sed -i "/address/s/127.0.0.1/${COREOS_PUBLIC_IPV4}/" /etc/sensu/conf.d/client.json
+	/opt/sensu/bin/sensu-client -d /etc/sensu/conf.d
+} 
 case $SENSU_ROLE in
 	"server") /opt/sensu/bin/sensu-server -d /etc/sensu/conf.d;;
 	"api") /opt/sensu/bin/sensu-api -d /etc/sensu/conf.d;;
-	"client") /opt/sensu/bin/sensu-client -d /etc/sensu/conf.d;;
+	"client") run-client;;
 	*) echo "no such role"
 esac
 
